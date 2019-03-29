166bcb7d9fb44d2bfc0cbc4a01d6421001f3c1b6
Bruce Wayne <admin@bige0.com>
2019-03-29 11:01:34 +0800

    Init project

---
+++ app/src/androidTest/java/com/bige0/noscam/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.bige0.noscam
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.bige0.noscam", appContext.packageName)
+    }
+}
