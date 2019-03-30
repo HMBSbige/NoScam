885cf4b6102d80db7ab0f4d50a49a462b3dff33b
Bruce Wayne <admin@bige0.com>
2019-03-30 15:33:38 +0800

    demo

---
+++ app/src/main/java/com/bige0/noscam/ListAdapter.kt
@@ -0,0 +1,33 @@
+package com.bige0.noscam
+
+import android.content.*
+import android.view.*
+import android.widget.*
+import kotlinx.android.synthetic.main.row_layout.view.*
+
+class ListAdapter(val context: Context, val list: ArrayList<SmsData>) : BaseAdapter()
+{
+	override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View
+	{
+		val view = LayoutInflater.from(context).inflate(R.layout.row_layout, parent, false)
+		view.sms_sender.text = list[position].senderName
+		view.sms_message.text = list[position].message
+		view.sms_date.text = list[position].date
+		return view
+	}
+
+	override fun getItem(position: Int): Any
+	{
+		return list[position]
+	}
+
+	override fun getItemId(position: Int): Long
+	{
+		return position.toLong()
+	}
+
+	override fun getCount(): Int
+	{
+		return list.size
+	}
+}
--- app/src/main/java/com/bige0/noscam/MainActivity.kt
+++ app/src/main/java/com/bige0/noscam/MainActivity.kt
@@ -1,13 +1,115 @@
 package com.bige0.noscam
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import android.*
+import android.content.*
+import android.content.pm.*
+import android.net.*
+import android.os.*
+import android.provider.*
+import android.support.v4.app.*
+import android.support.v7.app.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity()
 {
+	private val requestReadSms: Int = 2
+
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		if (ActivityCompat.checkSelfPermission(
+				this,
+				Manifest.permission.READ_SMS
+			) != PackageManager.PERMISSION_GRANTED
+		)
+		{
+			ActivityCompat.requestPermissions(
+				this,
+				arrayOf(Manifest.permission.READ_SMS),
+				requestReadSms
+			)
+		} else
+		{
+			setSmsMessages("", null)
+		}
+
+		all_sms.setOnClickListener {
+			setSmsMessages("", null)
+		}
+
+		inbox_sms.setOnClickListener {
+			setSmsMessages("inbox", null)
+		}
+
+		outbox_sms.setOnClickListener {
+			setSmsMessages("outbox", null)
+		}
+		sent_sms.setOnClickListener {
+			setSmsMessages("sent", null)
+		}
+		draft_sms.setOnClickListener {
+			setSmsMessages("draft", null)
+		}
+		one_number_sms.setOnClickListener {
+			setSmsMessages("", "address LIKE '${getString(R.string.helloworld)}'")
+		}
+	}
+
+	override fun onRequestPermissionsResult(
+		requestCode: Int,
+		permissions: Array<out String>,
+		grantResults: IntArray
+	)
+	{
+		if (requestCode == requestReadSms)
+			setSmsMessages("", null)
+	}
+
+	private fun setSmsMessages(uriString: String, selection: String?)
+	{
+		val smsList = ArrayList<SmsData>()
+
+		val cursor = contentResolver.query(
+			Uri.parse("content://sms/$uriString"),
+			null, selection, null, null
+		)
+
+		if (cursor.moveToFirst())
+		{
+			val nameID = cursor.getColumnIndex("address")
+			val messageID = cursor.getColumnIndex("body")
+			val dateID = cursor.getColumnIndex("date")
+
+			do
+			{
+				val dateString = cursor.getString(dateID)
+				smsList.add(
+					SmsData(
+						cursor.getString(nameID),
+						Date(dateString.toLong()).toString(),
+						cursor.getString(messageID)
+					)
+				)
+			} while (cursor.moveToNext())
+
+			cursor.close()
+			val adapter = ListAdapter(this, smsList)
+
+			sms_list_view.adapter = adapter
+		}
+	}
+
+	private fun goToAppSetting(context: Context)
+	{
+		val intent = Intent()
+		intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+		val uri = Uri.fromParts("package", context.packageName, null)
+		intent.data = uri
+		startActivityForResult(intent, 123)
+		// 回调自己设置
 	}
 }
